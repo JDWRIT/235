d1211486724c4032a8b7125834604f8575f13a17
Joshua DeWinter <jsd3713@rit.edu>
2024-11-17 16:55:35 -0500

    Add Project 2 Basic Functionality and Layout

--- HW-pixi-game-start/HW-pixi-game-start/js/main.js
+++ HW-pixi-game-start/HW-pixi-game-start/js/main.js
@@ -55,23 +55,273 @@ async function setup() {
   sceneHeight = app.renderer.height;
 
   // #1 - Create the `start` scene
+  startScene = new PIXI.Container();
+  stage.addChild(startScene);
 
   // #2 - Create the main `game` scene and make it invisible
+  gameScene = new PIXI.Container();
+  gameScene.visible = false;
+  stage.addChild(gameScene);
 
   // #3 - Create the `gameOver` scene and make it invisible
+  gameOverScene = new PIXI.Container();
+  gameOverScene.visible = false;
+  stage.addChild(gameOverScene);
 
   // #4 - Create labels for all 3 scenes
+  createLabelsAndButtons();
 
   // #5 - Create ship
+  ship = new Ship(assets.spaceship);
+  gameScene.addChild(ship);
 
   // #6 - Load Sounds
+  shootSound = new Howl({
+    src: ["sounds/shoot.wav"],
+  });
+
+  hitSound = new Howl({
+    src: ["sounds/hit.mp3"],
+  });
+
+  fireballSound = new Howl({
+    src: ["sounds/fireball.mp3"],
+  });
 
   // #7 - Load sprite sheet
 
   // #8 - Start update loop
+  app.ticker.add(gameLoop);
+  
 
   // #9 - Start listening for click events on the canvas
 
   // Now our `startScene` is visible
   // Clicking the button calls startGame()
+}
+
+function createLabelsAndButtons() {
+  let buttonStyle = {
+    fill: 0xff0000,
+    fontSize: 48,
+    fointFamily: "Futura",
+  };
+
+  // 1 set up startScene
+  // 1A make top start label
+  let startLabel1 = new PIXI.Text("Circle Blast!", {
+    fill: 0xffffff,
+    fontSize: 96,
+    ontFamily: "Futura",
+    stroke: 0xff0000,
+    strokeThickness: 6,
+  });
+  startLabel1.x = 50;
+  startLabel1.y = 120;
+  startScene.addChild(startLabel1);
+
+  // 1B make middle start label
+  let startLabel2 = new PIXI.Text("R U worthy .. ?", {
+    fill: 0xffffff,
+    fontSize: 32,
+    fontFamily: "Futura",
+    fontStyle: "italic",
+    stroke: 0xff0000,
+    strokeThickness: 6,
+  });
+  startLabel2.x = 185;
+  startLabel2.y = 300;
+  startScene.addChild(startLabel2);
+
+  // 1C make start game button
+  let startButton = new PIXI.Text("Enter, if you dare!", buttonStyle);
+  startButton.x = sceneWidth / 2 - startButton.width / 2;
+  startButton.y = sceneHeight - 100;
+  startButton.interactive = true;
+  startButton.buttonMode = true;
+  startButton.on("pointerup", startGame); //startGame is a funcion reference
+  startButton.on("pointerover", (e) => (e.target.alpha = 0.7)); // conccise arrow function with no brackets
+  startButton.on("pointerout", (e) => (e.currentTarget.alpha = 1.0)); //ditto
+  startScene.addChild(startButton);
+
+  // 2 set up gameScene
+  let textStyle = {
+    fill: 0xffffff,
+    fontSize: 18,
+    fontFamily: "Futura",
+    stroke: 0xff0000,
+    strokeThickness: 4,
+  };
+
+  // 2A make score label
+  scoreLabel = new PIXI.Text("", textStyle);
+  scoreLabel.x = 5;
+  scoreLabel.y = 5;
+  gameScene.addChild(scoreLabel);
+  increaseScoreBy(0);
+
+  // 2B make life label
+  lifeLabel = new PIXI.Text("", textStyle);
+  lifeLabel.x = 5;
+  lifeLabel.y = 26;
+  gameScene.addChild(lifeLabel);
+  decreaseLifeBy(0);
+
+  // 3 - set up `gameOverScene`
+  // 3A - make game over text
+  let gameOverText = new PIXI.Text("Game Over!\n        :-O", {
+    fill: 0xffffff,
+    fontSize: 64,
+    fontFamily: "Futura",
+    stroke: 0xff0000,
+    strokeThickness: 6,
+  });
+  gameOverText.x = sceneWidth / 2 - gameOverText.width / 2;
+  gameOverText.y = sceneHeight / 2 - 160;
+  gameOverScene.addChild(gameOverText);
+
+  // 3B - make "play again?" button
+  let playAgainButton = new PIXI.Text("Play Again?", buttonStyle);
+  playAgainButton.x = sceneWidth / 2 - playAgainButton.width / 2;
+  playAgainButton.y = sceneHeight - 100;
+  playAgainButton.interactive = true;
+  playAgainButton.buttonMode = true;
+  playAgainButton.on("pointerup", startGame); // startGame is a function reference
+  playAgainButton.on("pointerover", (e) => (e.target.alpha = 0.7)); // concise arrow function with no brackets
+  playAgainButton.on("pointerout", (e) => (e.currentTarget.alpha = 1.0)); // ditto
+  gameOverScene.addChild(playAgainButton);
+}
+
+function increaseScoreBy(value) {
+  score += value;
+  scoreLabel.text = `Score: ${score}`;
+}
+
+function decreaseLifeBy(value) {
+  life -= value;
+  life = parseInt(life); // convert to integer
+  lifeLabel.text = `Life:   ${life}%`;
+}
+
+function startGame() {
+  console.log("startGame called");
+  startScene.visible = false;
+  gameOverScene.visible = false;
+  gameScene.visible = true;
+  // app.view.onclick = fireBullet; // We'll get to this later
+  levelNum = 1;
+  score = 0;
+  life = 100;
+  increaseScoreBy(0);
+  decreaseLifeBy(0);
+  ship.x = 300;
+  ship.y = 550;
+  loadLevel();
+  // Unpause the game which allows the gameLoop and events to be active
+  setTimeout(() => {
+    paused = false;
+  }, 50);
+}
+
+function createCircles(numCircles = 10) {
+  for (let i = 0; i < numCircles; i++) {
+    let c = new Circle(10, 0xffff00);
+    c.x = Math.random() * (sceneWidth - 50) + 25;
+    c.y = Math.random() * (sceneWidth - 400) + 25;
+    circles.push(c);
+    gameScene.addChild(c);
+  }
+}
+
+function loadLevel(){
+  createCircles(levelNum * 5);
+}
+
+function end() {
+  paused = true;
+
+  // Clear out level
+  circles.forEach((c) => gameScene.removeChild(c)); // concise arrow function with no brackets and no return
+  circles = [];
+
+  bullets.forEach((b) => gameScene.removeChild(b));
+  bullets = [];
+
+  explosions.forEach((e) => gameScene.removeChild(e));
+  explosionTextures = [];
+
+  gameOverScene.visible = true;
+  gameScene.visible = false;
+}
+
+function gameLoop(){
+  if (paused) return; // keep this commented out for now
+
+  // #1 - Calculate "delta time"
+  let dt = 1 / app.ticker.FPS;
+  if (dt > 1 / 12) dt = 1 / 12;
+
+
+  // #2 - Move Ship
+  let mousePosition = app.renderer.events.pointer.global;
+  //ship.position = mousePosition;
+
+  let amt = 6 * dt; // at 60 FPS would move about 10% of distance per update
+  // lerp (linear interpolate) the x & y values with lerp()
+  let newX = lerp(ship.x, mousePosition.x, amt);
+  let newY = lerp(ship.y, mousePosition.y, amt);
+
+  // keep the ship on the screen with clamp()
+  let w2 = ship.width / 2;
+  let h2 = ship.height / 2;
+  ship.x = clamp(newX, 0 + w2, sceneWidth - w2);
+  ship.y = clamp(newY, 0 + h2, sceneHeight - h2);
+
+  // #3 - Move Circles
+  for (let c of circles) {
+    c.move(dt);
+    if (c.x <= c.radius || c.x >= sceneWidth - c.radius) {
+      c.reflectX();
+      c.move(dt);
+    }
+    if (c.y <= c.radius || c.y >= sceneHeight - c.radius) {
+      c.reflectY();
+      c.move(dt);
+    }
+  }
+
+  // #4 - Move Bullets
+
+
+  // #5 - Check for Collisions
+  for (let c of circles) {
+    // #5A - circles & bullets
+    // TODO
+
+    // #5B - circles & ship
+    if (c.isAlive && rectsIntersect(c, ship)) {
+      hitSound.play();
+      gameScene.removeChild(c);
+      c.isAlive = false;
+      decreaseLifeBy(20);
+    }
+  }
+
+  // #6 - Now do some clean up
+  // #6A - Cleanup bullets
+  bullets = bullets.filter((b) => b.isAlive);
+
+  // #6B - Cleanup circles
+  circles = circles.filter((c) => c.isAlive);
+
+  // #6C - get rid of explosions
+  explosions = explosions.filter((e) => e.playing);
+
+  // #7 - Is game over?
+  if (life <= 0) {
+    end();
+    return; // return here so we skip #8 below
+  }
+
+  // #8 - Load next level
 }
--- project2/JS/P2Main.js
+++ project2/JS/P2Main.js
@@ -1,11 +1,56 @@
-window.onload = (e) => {findData()};
+window.onload = (e) => {document.querySelector("#search").onclick = findData;
+                        document.querySelector("#option").onchange = selectionChange;
+                        optionSelector = document.querySelector("#option");
+                        optionSelection = optionSelector.value;
+                        searchTerm = document.querySelector("#searchterm");
+                        searchTerm.innerHTML = `Search Term -> <input type="text" size="20" maxlength="20" autofocus value="pikachu" />`;
+                        };
+
+let optionSelector = null;
+let optionSelection = null;
+let searchTerm = null;
+let pokemonList = null;
+
+function selectionChange(){
+    optionSelection = optionSelector.value
+
+    if (optionSelection == "pokemon")
+    {
+        searchTerm.innerHTML = `Search Term -> <input type="text" size="20" maxlength="20" autofocus value="pikachu" />`;
+    }
+    else if (optionSelection == "ability") {
+        searchTerm.innerHTML = `Ability options -> `
+        searchTerm.innerHTML += `<select>
+                                <option value="early-bird" selected>Early Bird</option>
+                                <option value="damp">Damp</option>
+                                <option value="chlorophyll">Chlorophyll</option>
+                            </select>`;
+        searchTerm.innerHTML += `<br>Result limit -> <input id="resultLimit" type="text" size="20" maxlength="20" autofocus value="5" />`
+    }
+    else {
+        searchTerm.innerHTML = `Type options ->`;
+        searchTerm.innerHTML += `<select>
+                                <option value="normal" selected>Normal</option>
+                                <option value="fire">Fire</option>
+                                <option value="ice">Ice</option>
+                            </select>`;
+        searchTerm.innerHTML += `<br>Result limit -> <input id="resultLimit" type="text" size="20" maxlength="20" autofocus value="5" />`
+    }
+}
 
 function findData(){
     const POKE_URL = "https://pokeapi.co/api/v2/";
 
     let url = POKE_URL;
 
-    url += "pokemon/ditto";
+    url += optionSelection;
+
+    let term = searchTerm.childNodes[1].value;
+    displayTerm = term;
+
+    term = term.trim();
+
+    url += "/" + term;
 
     getData(url);
 }
@@ -24,9 +69,66 @@ function getData(url){
 function dataLoaded(e){
     let xhr = e.target;
 
-    document.querySelector("#ExampleContent").innerHTML = xhr.responseText;
+    let obj = JSON.parse(xhr.responseText);
+    console.log(obj);
+
+    let returnable = "";
+
+    if (optionSelection == "pokemon") {
+        returnable = `<div id='pokemon'>`;
+        let pokemonImage = obj.sprites.front_default;
+        returnable += `<img src='${pokemonImage}' title='pokemon_image' /><br>`;
+        returnable += "Name: " + obj.name + `<br>`;
+        returnable += "HP: " + obj.stats[0].base_stat + `<br>`;
+        returnable += "Attack: " + obj.stats[1].base_stat + `<br>`;
+        returnable += "Defense: " + obj.stats[2].base_stat + `<br>`;
+        returnable += "Speed: " + obj.stats[5].base_stat + `<br>`;
+        returnable += `</div>`
+    }
+    else {
+        pokemonList = obj.pokemon;
+        
+        for (let i = 0; i < document.querySelector("#resultLimit").value && pokemonList.length; i++)
+        {
+            getPokemon(pokemonList[i].pokemon.url);
+        }
+    }
+
+    document.querySelector("#content").innerHTML = returnable;
 }
 
 function dataError(e){
     console.log("An error occured");
+}
+
+function getPokemon(url){
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = addPokemon;
+
+    xhr.onerror = dataError;
+
+    document.querySelector("#content").innerHTML = "";
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function addPokemon(e){
+    let xhr = e.target;
+
+    let obj = JSON.parse(xhr.responseText);
+    console.log(obj);
+
+    let returnable = `<div id='pokemon'>`;
+    let pokemonImage = obj.sprites.front_default;
+    returnable += `<img src='${pokemonImage}' title='pokemon_image' /><br>`;
+    returnable += "Name: " + obj.name + `<br>`;
+    returnable += "HP: " + obj.stats[0].base_stat + `<br>`;
+    returnable += "Attack: " + obj.stats[1].base_stat + `<br>`;
+    returnable += "Defense: " + obj.stats[2].base_stat + `<br>`;
+    returnable += "Speed: " + obj.stats[5].base_stat + `<br>`;
+    returnable += `</div>`
+    
+    document.querySelector("#content").innerHTML += returnable
 }
