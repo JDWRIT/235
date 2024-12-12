bbcfcc4e1434ba805fee1cc1fc460c1206a01612
Joshua DeWinter <jsd3713@rit.edu>
2024-12-12 14:28:24 -0500

    Add mech and selection

--- project3/JS/mtt-classes.js
+++ project3/JS/mtt-classes.js
@@ -7,11 +7,50 @@ class Tile extends PIXI.Sprite {
         this.height = 175;
         this.x = (this.tileNumber[0] * this.width) + (this.width / 2);
         this.y = (this.tileNumber[1] * this.height) + (this.height / 2);
-        this.speed = 500;
+        this.speed = 50;
+        this.interactive = true;
+        this.buttonMode = true;
+        this.on("click", (e) => (deselectAll())); // clicked
+        this.on("tap", (e) => (deselectAll())); // clicked mobile
     }
 
     move(dt = 1 / 60, direction = [0, 0]) {
         this.x += direction[0] * this.speed * dt;
         this.y += direction[1] * this.speed * dt;
-      }
+    }
+}
+
+class Mech extends PIXI.Sprite {
+    constructor(texture, tileNumber, team) {
+        super(texture);
+        this.tileNumber = tileNumber;
+        this.anchor.set(0.5, 0.5);
+        this.width = 150;
+        this.height = 150;
+        this.x = (this.tileNumber[0] * 175) + (this.width / 2);
+        this.y = (this.tileNumber[1] * 175) + (this.height / 2);
+        this.speed = 50;
+        this.team = team;
+        this.energyCapacity = 4;
+        this.energy = 3;
+        this.energyRegeneration = 3;
+        this.interactive = true;
+        this.buttonMode = true;
+        this.on("click", this.select); // clicked
+        this.on("tap", this.select); // clicked mobile
+    }
+
+    move(dt = 1 / 60, direction = [0, 0]) {
+        this.x += direction[0] * this.speed * dt;
+        this.y += direction[1] * this.speed * dt;
+    }
+
+    select() {
+        deselectAll();
+        displayMechData(this)
+    }
+
+    deselect() {
+        stopDisplayingMechData(this);
+    }
 }
--- project3/JS/mtt-main.js
+++ project3/JS/mtt-main.js
@@ -13,27 +13,42 @@ let sceneWidth, sceneHeight;
 let turn = "x";
 
 let board = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]];
+let onBoard = [];
+let inputs = [false, false, false, false]
 
-window.addEventListener("keydown", keyPressed);
+window.addEventListener("keydown", keyDown);
+window.addEventListener("keyup", keyUp);
 
 // The "Enter" key now functions like the search button
-function keyPressed(e) {
-    let dt = 1 / app.ticker.FPS;
-    if (dt > 1 / 12) dt = 1 / 12;
-
+function keyDown(e) {
     if (e.code == "KeyW") {
-        moveBoardElements(dt, [0, 10]);
+        inputs[0] = true;
     }
     if (e.code == "KeyA") {
-        moveBoardElements(dt, [10, 0]);
+        inputs[1] = true;
     }
     if (e.code == "KeyS") {
-        moveBoardElements(dt, [0, -10]);
+        inputs[2] = true;
     }
     if (e.code == "KeyD") {
-        moveBoardElements(dt, [-10, 0]);
+        inputs[3] = true;
     }
-}             
+}
+
+function keyUp(e) {
+    if (e.code == "KeyW") {
+        inputs[0] = false;
+    }
+    if (e.code == "KeyA") {
+        inputs[1] = false;
+    }
+    if (e.code == "KeyS") {
+        inputs[2] = false;
+    }
+    if (e.code == "KeyD") {
+        inputs[3] = false;
+    }
+}
 
 // Load all assets
 loadImages();
@@ -41,7 +56,11 @@ loadImages();
 async function loadImages() {
   // https://pixijs.com/8.x/guides/components/assets#loading-multiple-assets
   PIXI.Assets.addBundle("sprites", {
-    mttWhiteTile: "images/MTTTileWhite.png"
+    mttWhiteTile: "images/MTTTileWhite.png",
+    mttOMech: "images/MTTO.png",
+    mttXMech: "images/MTTX.png",
+    mttOMarker: "images/MTTTravelMarkerO",
+    mttXMarker: "images/MTTTravelMarkerX"
   });
 
   // The second argument is a callback function that is called whenever the loader makes progress.
@@ -69,6 +88,34 @@ async function setup() {
       stage.addChild(boardTile);
     }
   }
+
+  // Make mechs
+  let mechX = new Mech(assets.mttXMech, [0, 0], "X");
+  onBoard.push(mechX);
+  stage.addChild(mechX);
+  let mechO = new Mech(assets.mttOMech, [14, 14], "O");
+  onBoard.push(mechO);
+  stage.addChild(mechO);
+
+  app.ticker.add(gameLoop);
+}
+
+function gameLoop(){
+    let dt = 1 / app.ticker.FPS;
+    if (dt > 1 / 12) dt = 1 / 12;
+
+    if (inputs[0]) {
+        moveBoardElements(dt, [0, 10]);
+    }
+    if (inputs[1]) {
+        moveBoardElements(dt, [10, 0]);
+    }
+    if (inputs[2]) {
+        moveBoardElements(dt, [0, -10]);
+    }
+    if (inputs[3]) {
+        moveBoardElements(dt, [-10, 0]);
+    }
 }
 
 function moveBoardElements(dt, direction){
@@ -76,5 +123,24 @@ function moveBoardElements(dt, direction){
         for (let x = 0; x < 15; x++) {
             board[x][y].move(dt, direction);
         }
+    }
+    for (let thing in onBoard) {
+        onBoard[thing].move(dt, direction);
+    }
+}
+
+function displayMechData(mech){
+    console.log("displayMechData!");
+    console.log("Clicked: " + mech.team)
+}
+
+function stopDisplayingMechData(mech){
+    console.log("stopDisplayingMechData!");
+    console.log("Unclicked: " + mech.team)
+}
+
+function deselectAll(){
+    for (let thing in onBoard) {
+        onBoard[thing].deselect();
     }
 }
